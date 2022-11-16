480fb3b107d9af333c32a9e937808e4479cd063b
cjsyrzy <cjsyrzy@126.com>
2022-11-16 16:00:25 +0800

    first ok

--- main.c
+++ main.c
@@ -18,8 +18,8 @@
 /*********************
  *      DEFINES
  *********************/
-#define HOR_RES 189
-#define VER_RES 64
+#define HOR_RES 90
+#define VER_RES 49
 /**********************
  *      TYPEDEFS
  **********************/
@@ -27,8 +27,10 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
+static lv_disp_drv_t disp_drv;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t disp_buf1[HOR_RES * VER_RES];
+lv_color_t buf[HOR_RES * VER_RES];
 static lv_disp_t * dispplay = NULL;
 
 static void flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
@@ -36,34 +38,53 @@ static void flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_
     LV_UNUSED(area);
     LV_UNUSED(color_p);
 
-    int i = 0;
-    do
+    static int i,j;
+    for(j=area->y1; j<=area->y2; j++)
     {
-        if(disp_buf1[i].ch.blue)
+        for(i=area->x1; i<=area->x2; i++)
         {
-            putchar('*');
+            buf[j*HOR_RES + i].full = color_p->full;
+            color_p++;
+        }
+    }
+
+    
+    i = 0;
+    do
+    {   if(i%HOR_RES == 0)
+        printf("\r\n");
+
+        if(buf[i].full)
+        {
+            putchar('8');
         }
         else
         {
             putchar(' ');
         }
     } while (++ i < HOR_RES*VER_RES);
-    printf("\r\n");
 
     lv_disp_flush_ready(disp_drv);
 }
 
 static void hal_init(void)
 {
+    memset(buf, 0, sizeof(buf));
+
     lv_disp_draw_buf_init(&draw_buf, disp_buf1, NULL, HOR_RES * VER_RES);
 
-    static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.draw_buf = &draw_buf;
     disp_drv.flush_cb = flush_cb;
     disp_drv.hor_res = HOR_RES;
     disp_drv.ver_res = VER_RES;
+    // disp_drv.full_refresh = 1;
+    // disp_drv.direct_mode = 1;
     dispplay = lv_disp_drv_register(&disp_drv);
+    // if(dispplay != NULL)
+    // {
+    //     printf("create display ok\r\n");
+    // }
 
     // static lv_indev_drv_t indev_mouse_drv;
     // lv_indev_drv_init(&indev_mouse_drv);
@@ -86,20 +107,26 @@ static void hal_init(void)
 
 static void my_widget(void)
 {
-    lv_obj_t * label1 = lv_label_create(lv_scr_act());
-    lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
-    lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
-    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
-                      "and wrap long text automatically.");
-    lv_obj_set_width(label1, 150);  /*Set smaller width to make the lines wrap*/
-    lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
+    // lv_obj_t * label1 = lv_label_create(lv_scr_act());
+    // lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
+    // lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
+    // lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
+    //                   "and wrap long text automatically.");
+    // lv_obj_set_width(label1, 150);  /*Set smaller width to make the lines wrap*/
+    // lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
+    // lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
 
     lv_obj_t * label2 = lv_label_create(lv_scr_act());
     lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
-    lv_obj_set_width(label2, 150);
+    lv_obj_set_width(label2, 80);
     lv_label_set_text(label2, "It is a circularly scrolling text. ");
-    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 0);
+
+    // lv_obj_t *ta = lv_textarea_create(lv_scr_act());
+    // lv_textarea_set_text(ta, "ABCDE abcde");
+    // lv_obj_set_size(ta, 148, 40);
+    // lv_obj_align(ta, LV_ALIGN_CENTER, 0, 0);
+
 }
 
 /**********************
@@ -131,8 +158,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
         /* Periodically call the lv_task handler.
          * It could be done in a timer interrupt or an OS task too.*/
         lv_task_handler();
-        Sleep(100);
-        lv_tick_inc(100);
+        Sleep(10);
+        lv_tick_inc(10);
     }
     return 0;
 }
